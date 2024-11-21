1a8bde8f3639187767b85f6e3606bc9179ed19f9
KobeBerckmans <kobe.berckmans@student.ehb.be>
2024-11-21 10:19:50 +0100

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Kobe Berckmans on 21/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
